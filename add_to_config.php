bd03c8035c1f5f121d8a9e4f699282c2f5894306
mem03748 <77647155+mem03748@users.noreply.github.com>
2021-04-07 21:23:19 -0600

    Update add_to_config.php
    success and error are too common in the URL so I used the words 'right' and 'wrong' instead. Feel free to change it, but you also need to change $msg to 'right' and 'wrong' in each file.

--- add_to_config.php
+++ add_to_config.php
@@ -1,8 +1,8 @@
-function redirect($msg, $file, $answer)
+function redirect($file, $msg)
 {
-  if (!empty($msg)) {
-    //make sure that answer is either success or error
-    header("Location: " . $file . "?message=$msg" . "?answer=$answer");
+  if ($msg == 'wrong' || $msg == 'right') {
+    //make sure that answer is either right or wrong
+    header("Location: " . $file . "?answer=$msg");
   } else {
     echo 'there was an error.';
   }
@@ -12,7 +12,7 @@ function message()
 {
 
 
-  if (strpos($_SERVER['REQUEST_URI'], "success") == true) {
+  if (strpos($_SERVER['REQUEST_URI'], "right") == true) {
 ?>
     <div class="notification is-success">
       <h2 class="title is-2">Success!</h2>
@@ -20,7 +20,7 @@ function message()
   <?php
   }
 
-  if (strpos($_SERVER['REQUEST_URI'], "error") == true) {
+  if (strpos($_SERVER['REQUEST_URI'], "wrong") == true) {
   ?>
     <div class="notification is-danger">
       <h2 class="title is-2">There was an error.</h2>
